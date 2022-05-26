ace6e79d6333fc291a97fa0c002894a4fc77698e
Van Paul Dayag <dayag.vanpaul@gmail.com>
2022-05-26 19:16:26 +0800

    remove placeholder text

--- taskrecordsystem/main.py
+++ taskrecordsystem/main.py
@@ -8,8 +8,6 @@ from rich.text import Text
 from rich.padding import Padding
 
 console = Console()
-lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid."
-
 
 def menu():
     print("""
@@ -27,7 +25,7 @@ def menu():
 def main():
     while True:
         menu()
-    [console.print(Panel(Text(lorem, justify="left"), padding=(1, 5),
+    [console.print(Panel(Text("body text", justify="left"), padding=(1, 5),
                    title=x, title_align="left")) for x in db.test_data()]
 
 
