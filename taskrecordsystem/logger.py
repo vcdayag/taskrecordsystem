ae17c89b0ef50cb9353fe5411b556ae32c1eee92
Van Paul Dayag <dayag.vanpaul@gmail.com>
2022-05-31 22:01:15 +0800

    create config for different level of logging

--- taskrecordsystem/logger.py
+++ taskrecordsystem/logger.py
@@ -2,15 +2,21 @@ import logging
 from rich.logging import RichHandler
 
 
-def logger(l="DEBUG"):
-    logging.basicConfig(
-        level=l, format="%(message)s", handlers=[RichHandler()]
-    )
+def logger(l="INFO"):
+    if l == "INFO":
+        logging.basicConfig(
+            level=l, format="%(message)s", handlers=[RichHandler(show_time=False,show_level=False,show_path=False)]
+        )
+    elif l == "DEBUG":
+        logging.basicConfig(
+            level=l, format="%(message)s", handlers=[RichHandler()]
+        )
     return logging.getLogger("rich")
 
 
 if __name__ == '__main__':
     log = logger()
     log.debug("this is debug")
-    log.info("Hello, World!")
-    log.error("this is an error")
+    log.info("[bold green]Hello, World!",extra={"markup":True})
+    log.error("[bold red]this is an error",extra={"markup":True})
+    # log.info("Hello, World!")
