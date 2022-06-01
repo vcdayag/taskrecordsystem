95bfef7af17f09da6b3be129bd6dda1ff1e3b07c
Zenith Averi Cipriano <zvcipriano@up.edu.ph>
2022-06-01 21:28:59 +0800

    Updates, Mark as done, add task to a category DONE!
    STR_TO_DATE replaced with TIMESTAMP to include the time. Newly added
    features can still edit tuples no longer available. Category in task
    view only shows the id, planning to include the name to be user
    friendly if all of you agrees. View per day and month still accepts
    incomplete input. View tasks per category should be added.

--- taskrecordsystem/database.py
+++ taskrecordsystem/database.py
@@ -54,7 +54,7 @@ class DATABASE():
 
         try:
             self.cursor.execute(
-                "INSERT INTO task(title, details, deadline) VALUES (%s, %s, STR_TO_DATE(%s,'%Y-%m-%d'))", (title, details, deadline))
+                "INSERT INTO task(title, details, deadline) VALUES (%s, %s, TIMESTAMP(%s))", (title, details, deadline))
             self.connection.commit()
             log.info("[bold green]Successfully Added!", extra={"markup": True})
         except mariadb.Error as e:
@@ -71,9 +71,81 @@ class DATABASE():
         except mariadb.Error as e:
             log.error(e)
 
+    def mark_as_done(self, Id):
+        try:
+            self.cursor.execute(
+                "UPDATE task SET finished=1 WHERE task_id=%s", (Id))
+            self.connection.commit()
+            log.info("[bold green]Task Finished!", extra={"markup": True})
+        except mariadb.Error as e:
+            log.error(e)
+
+    def update_task_title(self, title, Id):
+        try:
+            self.cursor.execute(
+                "UPDATE task SET title=%s WHERE task_id=%s", (title, Id))
+            self.connection.commit()
+            log.info("[bold green]Successfully Edited!", extra={"markup": True})
+        except mariadb.Error as e:
+            log.error(e)
+
+    def update_task_details(self, details, Id):
+        try:
+            self.cursor.execute(
+                "UPDATE task SET details=%s WHERE task_id=%s", (details, Id))
+            self.connection.commit()
+            log.info("[bold green]Successfully Edited!", extra={"markup": True})
+        except mariadb.Error as e:
+            log.error(e)
+
+    def update_task_both(self, title, details, Id):
+        try:
+            self.cursor.execute(
+                "UPDATE task SET title=%s, details=%s WHERE task_id=%s", (title, details, Id))
+            self.connection.commit()
+            log.info("[bold green]Successfully Edited!", extra={"markup": True})
+        except mariadb.Error as e:
+            log.error(e)
+
+    def update_category_name(self, name, Id):
+        try:
+            self.cursor.execute(
+                "UPDATE category SET name=%s WHERE category_id=%s", (name, Id))
+            self.connection.commit()
+            log.info("[bold green]Successfully Edited!", extra={"markup": True})
+        except mariadb.Error as e:
+            log.error(e)
+
+    def update_category_description(self, description, Id):
+        try:
+            self.cursor.execute(
+                "UPDATE category SET description=%s WHERE category_id=%s", (description, Id))
+            self.connection.commit()
+            log.info("[bold green]Successfully Edited!", extra={"markup": True})
+        except mariadb.Error as e:
+            log.error(e)
+
+    def update_category_both(self, name, description, Id):
+        try:
+            self.cursor.execute(
+                "UPDATE category SET name=%s, description=%s WHERE category_id=%s", (name, description, Id))
+            self.connection.commit()
+            log.info("[bold green]Successfully Edited!", extra={"markup": True})
+        except mariadb.Error as e:
+            log.error(e)
+
+    def add_task_to_category(self, categoryId, taskId):
+        try:
+            self.cursor.execute(
+                "UPDATE task SET category_id=%s WHERE task_id=%s", (categoryId, taskId))
+            self.connection.commit()
+            log.info("[bold green]Successfully Added!", extra={"markup": True})
+        except mariadb.Error as e:
+            log.error(e)
+
     def delete_task(self, Id):
         try:
-            self.cursor.execute("DELETE FROM task WHERE task_id=%s", (Id,))
+            self.cursor.execute("DELETE FROM task WHERE task_id=%s", (Id))
             self.connection.commit()
             if self.cursor.rowcount == 0:
                 log.info("No task was deleted")
@@ -87,7 +159,7 @@ class DATABASE():
     def delete_category(self, Id):
         try:
             self.cursor.execute(
-                "DELETE FROM category WHERE category_id=%s", (Id,))
+                "DELETE FROM category WHERE category_id=%s", (Id))
             self.connection.commit()
             if self.cursor.rowcount == 0:
                 log.info("No task was deleted")
@@ -138,16 +210,16 @@ class DATABASE():
         return self.cursor.fetchall()
 
     def get_tasks(self):
-        return self.get_query("SELECT task_id, title, details, deadline, finished, category_id FROM task")
+        return self.get_query("SELECT * FROM task")
 
     def get_tasks_day(self, date):
-        return self.get_query("SELECT task_id, title, details, deadline, finished, category_id FROM task WHERE DATE_FORMAT(deadline, '%Y-%m-%d') = %s", (date,))
+        return self.get_query("SELECT * FROM task WHERE DATE_FORMAT(deadline, '%Y-%m-%d') = %s", (date,))
 
     def get_tasks_month(self, month):
-        return self.get_query("SELECT task_id, title, details, deadline, finished, category_id FROM task WHERE DATE_FORMAT(deadline, '%Y-%m') = %s", (month,))
+        return self.get_query("SELECT * FROM task WHERE DATE_FORMAT(deadline, '%Y-%m') = %s", (month,))
 
     def get_categories(self):
-        return self.get_query("SELECT category_id, name, description FROM category")
+        return self.get_query("SELECT * FROM category")
 
     def close(self):
         self.connection.close()
--- taskrecordsystem/main.py
+++ taskrecordsystem/main.py
@@ -70,7 +70,7 @@ def add_task():
     console.print("\n***Create New Task***")
     title = Prompt.ask("Title")
     details = Prompt.ask("Details")
-    deadline = Prompt.ask("Deadline (YYYY-MM-DD)")
+    deadline = Prompt.ask("Deadline (YYYY-MM-DD hh:mm)")
     db.add_task(title, details, deadline)
 
 
@@ -92,8 +92,109 @@ def delete_category():
     Id = Prompt.ask("Category Id")
     db.delete_category(Id)
 
+def mark_as_done():
+    console.print("\n***Mark Task as Done***")
+    taskid = Prompt.ask("Task Id")
+    db.mark_as_done(taskid)
 
-CHOICES = [str(x) for x in range(9)]
+def update_task():
+    TASKCHOICES = [str(x) for x in range(4)]
+
+    while True:
+        console.print("""\n***Update a Task***
+[0] Cancel Update
+[1] Title only
+[2] Details only
+[3] Both""")
+        holder = IntPrompt.ask("Choice", choices=TASKCHOICES, show_choices=False)
+
+        match holder:
+            case 0: 
+                console.print("Update Cancelled!")
+                break
+            case 1: 
+                task_title_only()
+                break
+            case 2: 
+                task_details_only()
+                break
+            case 3: 
+                task_both()
+                break
+
+
+def task_title_only():
+    console.print("\n***Task to be Edited***")
+    taskid = Prompt.ask("Task Id")
+    newTitle = Prompt.ask("New Title")
+    db.update_task_title(newTitle, taskid)
+
+def task_details_only():
+    console.print("\n***Task to be Edited***")
+    taskid = Prompt.ask("Task Id")
+    newDetails = Prompt.ask("New Details")
+    db.update_task_details(newDetails, taskid)
+
+def task_both():
+    console.print("\n***Task to be Edited***")
+    taskid = Prompt.ask("Task Id")
+    newTitle = Prompt.ask("New Title")
+    newDetails = Prompt.ask("New Details")
+    db.update_task_both(newTitle, newDetails, taskid)
+
+def update_category():
+    CATEGORYCHOICES = [str(x) for x in range(4)]
+
+    while True:
+        console.print("""\n***Update a Category***
+[0] Cancel Update
+[1] Name only
+[2] Description only
+[3] Both""")
+        holder = IntPrompt.ask("Choice", choices=CATEGORYCHOICES, show_choices=False)
+
+        match holder:
+            case 0: 
+                console.print("Update Cancelled!")
+                break
+            case 1: 
+                category_name_only()
+                break
+            case 2: 
+                category_description_only()
+                break
+            case 3: 
+                category_both()
+                break
+
+
+def category_name_only():
+    console.print("\n***Category to be Edited***")
+    categoryid = Prompt.ask("Category Id")
+    newName = Prompt.ask("New Name")
+    db.update_category_name(newName, categoryid)
+
+def category_description_only():
+    console.print("\n***Category to be Edited***")
+    categoryid = Prompt.ask("Category Id")
+    newDescription = Prompt.ask("New Description")
+    db.update_category_Description(newDescription, categoryid)
+
+def category_both():
+    console.print("\n***Category to be Edited***")
+    categoryid = Prompt.ask("Category Id")
+    newTitle = Prompt.ask("New Title")
+    newDescription = Prompt.ask("New Description")
+    db.update_category_both(newTitle, newDescription, categoryid)
+
+def add_task_to_category():
+    console.print("\n***Add a Task to a Category***")
+    taskid = Prompt.ask("Task Id")
+    categoryid = Prompt.ask("Category Id")
+    db.add_task_to_category(categoryid, taskid)
+
+
+CHOICES = [str(x) for x in range(13)]
 MAINMENUCOMMANDS = """[0] Quit
 [1] Add Task
 [2] View Tasks
@@ -103,6 +204,10 @@ MAINMENUCOMMANDS = """[0] Quit
 [6] Delete Category
 [7] View Tasks (Day)
 [8] View Tasks (Month)
+[9] Mark Task as Done
+[10] Update Task
+[11] Update Category
+[12] Add Task to Category
 """
 
 
@@ -120,6 +225,10 @@ def menu():
             case 6: delete_category()
             case 7: view_tasks("DAY")
             case 8: view_tasks("MONTH")
+            case 9: mark_as_done()
+            case 10: update_task()
+            case 11: update_category()
+            case 12: add_task_to_category()
             case 0:
                 console.print("Goodbye!")
                 break
